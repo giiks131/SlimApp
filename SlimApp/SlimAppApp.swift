ebcdb42dff6a1fdfeb88508cac1c737b89744b86
Alex Sh <giiks131@gmail.com>
2023-01-10 16:29:41 +0100

    Initial Commit

---
+++ SlimApp/SlimAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimAppApp.swift
+//  SlimApp
+//
+//  Created by Aleksandr Shapovalov on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SlimAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
