00dc41046432a94c452b4e82fe313e2cfbce6206
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-01-11 16:14:57 +0100

    Designed day picker to select or deselect days of the week

--- SlimApp/DayPicker/DayPicker.swift
+++ SlimApp/DayPicker/DayPicker.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct DayPicker: View {
-    
+    @Binding var days: [Weekday]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(Weekday.allCases, id: \.self) { day in
+                Button(day.abbrv) {
+                    select(day)
+                }
+                .foregroundColor(isSelected(day) ? .blue : .primary)
+            }
+        }
+    }
+    
+    func isSelected(_ day: Weekday) -> Bool {
+        days.contains(day)
+    }
+    
+    func select(_ day: Weekday) {
+        if let index = days.firstIndex(of: day) {
+            days.remove(at: index)
+        } else {
+            days.append(day)
+        }
     }
 }
 
 struct DayPicker_Previews: PreviewProvider {
     static var previews: some View {
-        DayPicker()
+        DayPicker(days: .constant([]))
     }
 }
