28e7038276bbbec17744917f8099f271e4284d2d
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-01-11 15:47:15 +0100

    Added weekday enum to be used in workout day selection

---
+++ SlimApp/DayPicker/DayPicker.swift
@@ -0,0 +1,21 @@
+//
+//  DayPicker.swift
+//  SlimApp
+//
+//  Created by Ahmed Mgua on 11/01/23.
+//
+
+import SwiftUI
+
+struct DayPicker: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        DayPicker()
+    }
+}
---
+++ SlimApp/DayPicker/Weekday.swift
@@ -0,0 +1,12 @@
+//
+//  Weekday.swift
+//  SlimApp
+//
+//  Created by Ahmed Mgua on 11/01/23.
+//
+
+import Foundation
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+}
