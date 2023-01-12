e5dd379de4eeffe032f36ad97c7d38ec4a535f7a
Mikhail Borisov <giiks131@gmail.com>
2023-01-12 14:19:44 +0100

    Fixes after Review
    Clean code for HomeView and fix background issue

--- SlimApp/HomeView.swift
+++ SlimApp/HomeView.swift
@@ -9,15 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
+        NavigationView {
+            ZStack {
             Color("Background")
                 .ignoresSafeArea()
-            NavigationView {
                 ScrollView {
                     content
-                        
                 }
-
                 .navigationTitle("Clients")
                 .toolbar {
                     Button {
@@ -31,19 +29,9 @@ struct HomeView: View {
     }
 }
 
-
 extension HomeView {
     var content: some View {
         VStack(alignment: .leading, spacing: 0) {
-
-//            Text("Clients")
-//                .font(.system(size: 34, weight: .bold))
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .padding(.horizontal, 20)
-
-
-//            Divider()
-
             ForEach(0 ..< 5) { item in
                 VStack(spacing: 15) {
                     ClientCard()
