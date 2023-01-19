3b9fb5ef40cba43ee9162012c0dacf91f1f504a5
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-01-19 17:56:47 +0100

    Implemented new client service

--- SlimApp/NewClient/NewClientService.swift
+++ SlimApp/NewClient/NewClientService.swift
@@ -10,3 +10,10 @@ import Spidey
 protocol NewClientServiceProtocol {
     func addNew(client: Client.New) async throws -> Client
 }
+
+struct NewClientService: NewClientServiceProtocol {
+    func addNew(client: Client.New) async throws -> Client {
+        try await NetworkManager()
+            .performRequest(endpoint: .newClient, authType: .none, payload: client, response: Client.self)
+    }
+}
