a6cb1802648cb177c34daf49f257d14dbb44add1
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-02-01 16:43:25 +0100

    Added methods for saving and deleting user from Keychain to user store

--- SlimApp/UserStore.swift
+++ SlimApp/UserStore.swift
@@ -9,4 +9,23 @@ import Foundation
 
 final class UserStore: ObservableObject {
     @Published private(set) var trainer: Trainer? = try? KeychainManager.load(key: .trainer)
+    @Published private(set) var error: KeychainManager.KeychainError?
+
+    func save(_ user: Trainer) {
+        do {
+            try KeychainManager.save(key: .trainer, data: user)
+            trainer = user
+        } catch {
+            self.error = error as? KeychainManager.KeychainError
+        }
+    }
+
+    func deleteUser() {
+        do {
+            try KeychainManager.delete(key: .trainer)
+            trainer = nil
+        } catch {
+            self.error = error as? KeychainManager.KeychainError
+        }
+    }
 }
